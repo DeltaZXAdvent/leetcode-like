c3143bc9ca441e03a9f86d713a050c29899f60da
deltazx <dzxdick206@163.com>
2024-08-02 00:02:23 +0800

    does not compile, wrong linked list implementation

--- yankao/2-3-7-1.rs
+++ yankao/2-3-7-1.rs
@@ -1,29 +1,23 @@
-/*
- * RUST BEGINNER
- */
-// Rust pointers
-// Requirements:
-// 1. Lifetime of Box? What if in multithreaded programs it's deleted?
-//    We don't consider the lifetime of Box?
-// 2. Access elements of a tuple?
-//    tuple.0
-// 3. Access fields in a struct?
-//    How do I which enumeration it is?
-//    enum.discriminant ()
-// 4. Box type to normal type?
-//    *b
-
-// The first element cannot be deleted
+// This is wrong implementation of linked list
+// A linked list should support owned nodes,
+// because the referenced nodes have determined lifetime, I guess.
+//
+// So there must be a mechanic in Rust that adds multiple owernship to an object,
+// and drop them (not by constructing the object passed with values).
+// So try Option, if we could modify Option, then we could possibly drop and takes multiple ownership.
 #[derive (Debug)]
 enum ListNode<'a> {
-    Cons (i32, &'a mut Box<ListNode<'a>>),
+    Cons (i32, &'a mut Box<ListNode<'a>>), // the reference is similar to a modifiable pointer
     Nil
 }
-// type List<'a> = &'a ListNode;
-// struct List<'a> { first: &'a ListNode }
+// Using Option is also OK, to challenge I tried copying the example for the Rust Book,
+// to use Nil; though this is a little bit illogical, Because a valid node should have some data
+//
+// To support empty lists, and avoid head nodes, let's use a reference to a ListNode for the list type;
+// This reference is also mutable, otherwise the first node cannot be popped;
+// Or don't use a function, use a method;
+// 
 use ListNode::{Cons, Nil};
-use std::ops::Deref;
-use std::ops::DerefMut;
 fn del_vals<'a> (list: &'a mut &'a mut Box<ListNode<'a>>, x: i32) {
     match &mut ***list {
 	Cons (v, b) => {
@@ -39,17 +33,18 @@ fn del_vals<'a> (list: &'a mut &'a mut Box<ListNode<'a>>, x: i32) {
 	Nil => {},
     }
 }
-fn main () {
-    let mut l0 = Box::new (ListNode::Nil);
-    let mut l1 = Box::new (ListNode::Cons (1, &mut l0));
-    let mut l2 = Box::new (ListNode::Cons (2, &mut l1));
-    let mut l3 = Box::new (ListNode::Cons (3, &mut l2));
-    let mut l4 = Box::new (ListNode::Cons (1, &mut l3));
-    let mut l5 = Box::new (ListNode::Cons (1, &mut l4));
 
-    let mut L = &mut l5;
-    let mut LL = &mut L;
-    del_vals (LL, 1);
-    // println!("{L:?}");
-    println!("hel");
+fn main () {
+    // Idk other ways to do this, a temporary value must be bound to a variable to make it last longer?
+    let mut l0 = Box::new (Nil);
+    let mut l1 = Box::new (Cons (1, &mut l0));
+    let mut l2 = Box::new (Cons (2, &mut l1));
+    let mut l3 = Box::new (Cons (3, &mut l2));
+    let mut l4 = Box::new (Cons (1, &mut l3));
+    let mut l5 = Box::new (Cons (1, &mut l4));
+    let mut l = &mut l5;
+    let mut ll = &mut l;
+    del_vals (ll, 1);
+    
+    println!("{ll:?}");		// errors
 }
