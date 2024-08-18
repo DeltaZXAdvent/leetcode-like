863721188248679eff47e3ffb2d4ea3304939573
deltazx <dzxdick206@163.com>
2024-08-18 20:22:27 +0800

    rust queues, nice

---
+++ yankao/2.2.3.6.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <time.h>
+#include <assert.h>
+#include <stdlib.h>
+void reverse (int *begin, int *end)
+{
+  while (begin < end)
+    {
+      int tmp = *begin;
+      *begin++ = *end;
+      *end-- = tmp;
+    }
+}
+
+void swap_multiple (int *begin, int *mid, int *end)
+{
+  reverse (begin, end);
+  reverse (begin, mid);
+  reverse (mid, end);
+}
+
+int gcd (int a, int b)
+{
+  if (b == 0)
+    return a;
+  return gcd (b, a - a / b * b);
+}
+
+void swap_multiple_direct (int *begin, int *mid, int *end)
+{
+  int m = mid - begin,
+    n = end - mid,
+    k = gcd (m, n);
+  for (int *start = begin; start != begin + k; ++start)
+    {
+      int *cur = start,
+	tmp1 = *cur,
+	tmp2,
+	use_tmp1 = 1;
+      do
+	{
+	  cur = cur < mid ?
+	    cur + n : cur - m;
+	  if (use_tmp1)
+	    { tmp2 = *cur; *cur = tmp1; }
+	  else
+	    { tmp1 = *cur; *cur = tmp2; }
+	  use_tmp1 = !use_tmp1;
+	}
+      while (cur != start);
+    }
+}
+
+void print_vec (int *begin, int *end)
+{
+  printf ("[");
+  for (; begin < end; ++begin)
+    printf ("%d ", *begin);
+  printf ("]\n");
+}
+
+void my_getline ()
+{
+  int ch;
+  while ((ch = getchar ()) != '\n')
+    assert (ch != EOF);
+}
+
+clock_t cp;
+#define timer(exp) do {							\
+    cp = clock ();							\
+    printf ("%s", "press to start timer for task "#exp);		\
+    my_getline ();							\
+    (exp);								\
+    clock_t new_cp = clock ();						\
+    printf ("time spent: %f seconds\n", (double) (new_cp - cp) / CLOCKS_PER_SEC); \
+    cp = new_cp;							\
+  } while (0)
+
+int main (int argc, char *argv[])
+{
+  assert (argc == 3);
+  size_t m = atol (argv[1]),
+    n = atol (argv[2]);
+  printf ("m: %zu, n: %zu, sizeof (int) * (m + n): %zu\n", m, n, m + n, sizeof (int) * (m + n));
+  int *vec = malloc (sizeof (int) * (m + n));
+  for (size_t i = 0; i < m + n; ++i)
+    vec[i] = i;
+  timer (swap_multiple (vec, vec + m, vec + m + n));
+  my_getline ();
+  /* print_vec (vec, vec + m + n); */
+  timer (swap_multiple_direct (vec, vec + m, vec + m + n));
+  my_getline ();
+  /* print_vec (vec, vec + m + n); */
+  timer (gcd (m, n));
+  free (vec);
+  return 0;
+}
+
+// TODO
+// 这个是因为想纯使用C++库，
+// 我对优化和汇编都不了解，但我想的是第二种算法的分支也很简单，不应该需要跳转指令吧，只需要把两条分支都条件执行一遍；
+// 第二种算法是内存访问少一半，把每个元素移到对应位置，然后把相应位置元素再移走。3
--- yankao/sec-queue.rs
+++ yankao/sec-queue.rs
@@ -1,6 +1,8 @@
 use std::collections::VecDeque;
 use std::ptr;
-fn reverse (deque: &mut VecDeque<i32>) {
+type Ele = i32;
+type Vdq = VecDeque<Ele>;
+fn reverse (deque: &mut Vdq) {
     let mut stack = VecDeque::new ();
     loop {
 	match deque.pop_front () {
@@ -24,7 +26,7 @@ struct Queue {
 
 #[derive (Debug)]
 struct Node {
-    data: i32,
+    data: Ele,
     next: Option<Box<Node>>
 }
 
@@ -32,7 +34,7 @@ impl Queue {
     fn new () -> Self {
 	return Queue { head: None, tail: ptr::null_mut () };
     }
-    fn enqueue (&mut self, value: i32) {
+    fn enqueue (&mut self, value: Ele) {
 	let mut new_node = Box::new (Node { data: value, next: None });
 	let node_ptr = &mut *new_node as *mut Node;
 	if self.head.is_none () {
@@ -44,7 +46,7 @@ impl Queue {
 	}
 	self.tail = node_ptr;
     }
-    fn dequeue (&mut self) -> i32 {
+    fn dequeue (&mut self) -> Ele {
 	if let Some (mut head) = self.head.take () {
 	    let pop = head.data;
 	    self.head = head.next.take ();
@@ -56,7 +58,139 @@ impl Queue {
     }
 }
 
+struct StackBounded {
+    s: Vdq,
+    maxlen: usize
+}
+impl StackBounded {
+    fn new (l: usize) -> Self { Self { s: Vdq::new (), maxlen: l } }
+    fn push (&mut self, value: Ele) { if !self.full () { self.s.push_back (value); } else { panic!(); } }
+    fn pop (&mut self) -> Ele { if !self.empty () { return self.s.pop_back ().unwrap (); } else { panic!(); } }
+    fn empty (&self) -> bool { self.s.is_empty () }
+    fn full (&self) -> bool { self.s.len () == self.maxlen }
+}
+
+struct Queue2stacks {
+    s1: StackBounded,
+    s2: StackBounded
+}
+impl Queue2stacks {
+    fn new (l: usize) -> Self {
+	let ret = Self { s1: StackBounded::new (l), s2: StackBounded::new (l) };
+	assert!(ret.s1.maxlen <= ret.s2.maxlen); // to make s2 able to hold all of s1
+	assert!(ret.s1.maxlen >= ret.s2.maxlen); // to make all of s2 useful
+	ret
+    }
+    fn enqueue (&mut self, value: Ele) {
+	if self.s1.full () {
+	    if !self.s2.empty () { panic!(); }
+	    else {
+		while !self.s1.empty () {
+		    self.s2.push (self.s1.pop ());
+		}
+	    }
+	}
+	self.s1.push (value);
+    }
+    fn dequeue (&mut self) -> Ele {
+	if self.s2.empty () {
+	    if self.s1.empty () { panic!(); }
+	    else {
+		while !self.s1.empty () {
+		    self.s2.push (self.s1.pop ());
+		}
+	    }
+	}
+	self.s2.pop ()
+    }
+    fn empty (&self) -> bool {
+	self.s1.empty () && self.s2.empty ()
+    }
+}
+
+#[derive (Debug)]
+struct LinkedCircularQueue {
+    head: *mut Nodep,
+    tail: *mut Nodep
+}
+struct Nodep {
+    data: Ele,
+    next: *mut Nodep
+}
+impl Nodep {
+    fn new (value: Ele) -> Self {
+	Self { data: value, next: ptr::null_mut () }
+    }
+}
+impl LinkedCircularQueue {
+    fn new () -> Self {
+	let p = Box::into_raw (Box::new (Nodep::new (0)));
+	unsafe { (*p).next = p; }
+	Self { head: p, tail: p }
+    }
+    fn enqueue (&mut self, value: Ele) {
+	unsafe {
+	    if (*self.tail).next == self.head {
+		let p = Box::into_raw (Box::new (Nodep::new (value)));
+		(*p).next = (*self.tail).next;
+		(*self.tail).next = p; 
+		self.tail = p;
+	    } else {
+		self.tail = (*self.tail).next;
+		(*self.tail).data = value;
+	    }
+	}
+    }
+    fn dequeue (&mut self) -> Ele {
+	if self.head == self.tail { panic!(); }
+	else {
+	    unsafe {
+		self.head = (*self.head).next;
+		return (*self.head).data;
+	    }
+	}
+    }
+    fn drop (self) {
+	let p = self.head;
+	let mut q = p;
+	loop {
+	    unsafe {
+		let r = q;
+		q = (*q).next;
+		drop (Box::from_raw (r));
+	    }
+	    if q == p { break; }
+	}
+    }
+}
+
 fn main () {
+    let mut r = LinkedCircularQueue::new ();
+    for i in 0..8 {
+	r.enqueue (i);
+	println!("{i:?}");
+	println!("{r:?}");
+    }
+    println!("{r:?}");
+    for i in 0..8 {
+	let out = r.dequeue ();
+	println!("{out:?}");
+	println!("{r:?}");
+    }
+    r.drop ();
+    return;
+
+    let mut qq = Queue2stacks::new (4);
+    for i in 1..9 {
+	qq.enqueue (i);
+	println!("{i:?}");
+    }
+    for i in 1..9 {
+	let out = qq.dequeue ();
+	println!("{out:?}");
+    }
+    return;
+    
     let mut q = Queue::new ();
 
     for i in 1..8 {
@@ -85,4 +219,5 @@ fn main () {
     }
 
     println!("{q:?}");
+
 }
