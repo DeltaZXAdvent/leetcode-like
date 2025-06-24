d6421ba51d76c778f0cab402de4cc3deb7725059
deltazx <dzxdick206@163.com>
2025-06-24 11:59:35 +0800

    new contest

--- codeforces/Gangsta.cpp
+++ codeforces/Gangsta.cpp
@@ -1,21 +1,75 @@
+/**
+ * \file
+ * <https://codeforces.com/contest/2121/problem/G>
+ *
+ * Let \\( f(l-1,r)=f(s_l s_{l+1} \\ldots s_r) \\),
+ * If \\(f(p,q),f(q,r),f(p,r)\\) have a relationship,
+ * then we can omit computation repetitions.
+ *
+ * When \\(f(l,r) = \sum_{i=l+1}^{r}s_i\\),
+ * we already have used that in problem F.
+ *
+ * In this case, \\( f(l,r)=\max \\{c_0(l,r), c_1(l,r)\\} \\),
+ * where \\( c_0, c_1 \\) are the numbers of 0's and 1's
+ * in the segment, as defined in the tutorial.
+ *
+ * \\( c(p,q)=c(p,r)-c(q,r)\\). Let \\( c(0,q)=c(q) \\),
+ * then \\( f(l,r)=\max \\{ c_0(r)-c_0(l), c_1(r)-c_1(l)\\} \\).
+ *
+ * If some maximums are always chosen from the first,
+ * we can group them and eliminate them.
+ * Kinda hard to describe, but let's write \\( \max \\) in
+ * absolute values. Then we have only to discuss the original
+ * values are positive or non-positive.
+ *
+ * \\(2f(l,r)=r-l+|(c_1-c_0)(r)-(c_1-c_0)(l)|\\).
+ * The summation of the right term can be optimized using sorting.
+ * Let \\( \Delta c=c_1-c_0 \\), \\( \Delta'c(i) \\) be the ordered version,
+ * Then,
+ * \\[ 2\sum_{0\le l<r\le n}f(l,r)=\frac{n(n+1)(n+2)}{6}
+ * +\sum_{i=0}^n (2i-n)\Delta'c(i) \\]
+ */
 #include <bits/stdc++.h>
 static_assert (sizeof (int) * CHAR_BIT >= 32);
 typedef long long llong;
 using namespace std;
 #define FOR2(i, n) FOR (i, 0, n)
 #define FOR3(i, p, q) for (int i = p; i < q; ++i)
+#define DUMP(x) (string(#x) + ": " + to_string(x))
 
-/**
- * https://codeforces.com/contest/2121/problem/G
- * TODO
- */
+string& to_string(string& x) { return x; }
 
 int main (int argc, char *argv[])
 {
   int t; cin >> t;
   while (t--)
     {
-      
+      int n; cin >> n;
+      cin.ignore();
+      string s;
+      getline(cin, s);
+      // cerr << DUMP(n) << ' ' << DUMP(s) << '\n';
+      vector<int> ones_sub_zeroes(n + 1);
+      int ones = 0, zeroes = 0;
+      for (int i = 0;; ++i)
+	{
+	  ones_sub_zeroes[i] = ones - zeroes;
+	  if (i == n) break;
+	  ++(s[i] == '1' ? ones : zeroes);
+	}
+      // cerr << "ones_sub_zeroes: ";
+      // FOR3 (i, 0, n + 1) cerr << ones_sub_zeroes[i] << (i == n ? '\n' : ' ');
+      sort(ones_sub_zeroes.begin(), ones_sub_zeroes.end());
+      // cerr << "ones_sub_zeroes: ";
+      // FOR3 (i, 0, n + 1) cerr << ones_sub_zeroes[i] << (i == n ? '\n' : ' ');
+      llong left_sum = (llong) n * (n + 1) * (n + 2) / 6;
+      llong right_sum = 0;
+      FOR3 (i, 0, n + 1)
+	{
+	  right_sum += ((llong) 2 * i - n) * ones_sub_zeroes[i];
+	}
+      llong ans = (left_sum + right_sum) / 2;
+      cout << ans << '\n';
     }
   return 0;
 }
--- codeforces/Yamakasi.cpp
+++ codeforces/Yamakasi.cpp
@@ -4,10 +4,24 @@ typedef long long llong;
 using namespace std;
 #define FOR2(i, n) FOR (i, 0, n)
 #define FOR3(i, p, q) for (int i = p; i < q; ++i)
+#define DUMP(x) (string(#x) + ": " + to_string(x))
 
 /**
  * <https://codeforces.com/contest/2121/problem/F>
- * TODO
+ *
+ * Already read the tutorial.
+ * 
+ * The first step is to determine the intervals that fulfill the maximum
+ * If the right boundary is determined, the left boundary lies in a interval
+ * The interval's left and right boundaries both move to the right.
+ *
+ * Thus we can optimize the choice of left boundaries
+ * that fulfill the summation condition.
+ *
+ * Summation of subsegments can be reduced to subtraction
+ * of partial sums.
+ *
+ * The result program can be tagged "three pointers".
  */
 
 int main (int argc, char *argv[])
@@ -17,10 +31,49 @@ int main (int argc, char *argv[])
     {
       int n; llong s; int x;
       cin >> n >> s >> x;
-      vector<int> a(n);
-      FOR2 (i, n) cin >> a[i];
-
-      
+      // cerr << DUMP(n) << ' ' << DUMP(s) << ' ' << DUMP(x) << '\n';
+      vector<int> a(n + 1);
+      FOR3 (i, 1, n + 1) cin >> a[i];
+      vector<llong> sum(n + 1);
+      sum[0] = 0;
+      FOR3 (i, 1, n + 1) sum[i] = sum[i - 1] + a[i];
+      int begin_from = 0, begin_to = 0;
+      map<llong, int> partial_sum_cts; // in the range [begin_from, begin_to)
+      llong pair_cts = 0;
+      int end = 0;
+      while (end < n)
+	{
+	  if (a[end + 1] == x)
+	    {
+	      while (begin_to != end + 1)
+		{
+		  ++partial_sum_cts[sum[begin_to]];
+		  ++begin_to;
+		}
+	    }
+	  else if (a[end + 1] > x)
+	    {
+	      begin_to = begin_from = end + 1;
+	      partial_sum_cts.clear();
+	    }
+	  else
+	    ;
+	  ++end;
+	  // cerr << DUMP(sum[end]) << ' '
+	  //      << DUMP(sum[end] - s) << ' ';
+	  auto iter = partial_sum_cts.find(sum[end] - s);
+	  if (iter != partial_sum_cts.end())
+	    {
+	      // cerr << DUMP(iter->second);
+	      pair_cts += iter->second;
+	    }
+	  // cerr << '\n';
+	  // cerr << DUMP(end) << ' '
+	  //      << DUMP(begin_from) << ' '
+	  //      << DUMP(begin_to) << ' '
+	  //      << DUMP(pair_cts) << '\n';
+	}
+      cout << pair_cts << '\n';
     }
   return 0;
 }
